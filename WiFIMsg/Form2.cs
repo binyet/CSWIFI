74cd0811ff0fec286097b6e601c81d5d03ecc28f
binyet <1244257841@qq.com>
2017-08-09 19:42:04 +0800

    1755

--- WiFIMsg/Form2.cs
+++ WiFIMsg/Form2.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace WiFIMsg
@@ -7,6 +12,8 @@ namespace WiFIMsg
     public partial class Form2 : Form
     {
         IntPtr h;
+        private static Encoding encode = Encoding.Default;
+
         public Form2()
         {
             InitializeComponent();
@@ -14,20 +21,15 @@ namespace WiFIMsg
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //serialPort1.Open();
-            //serialPort1.WriteLine(textBox1.Text.ToString());
-            //serialPort1.Close();
             string str = textBox1.Text;
             IntPtr i = Marshal.StringToHGlobalAuto(str);
             WIn32Api.PostMessage(h, WIn32Api.UM_1, 0, i);
-            
+
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //serialPort1.Close();
-            //serialPort1.PortName = "COM3";
-            //serialPort1.BaudRate = 9600;
             var f = new NewWindow();
             h = f.Handle;
             f.Show();
@@ -35,7 +37,88 @@ namespace WiFIMsg
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            new Thread(connSocket).Start();
+        }
 
+        private void connSocket()
+        {
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(new IPEndPoint(IPAddress.Any, 8888));
+            listenSocket.Listen(100);
+            while (true)
+            {
+                Socket acceptSocket = listenSocket.Accept();
+                string receiceData = Receive(acceptSocket, 1000);
+                acceptSocket.Send(encode.GetBytes("ok"));
+                DestroySocket(acceptSocket); //import 
+            }
+        }
+        /// <summary>  
+        /// 发送数据  
+        /// </summary>  
+        /// <param name="host"></param>  
+        /// <param name="port"></param>  
+        /// <param name="data"></param>  
+        /// <returns></returns>  
+        public static string Send(string host, int port, string data)
+        {
+            string result = string.Empty;
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(host, port);
+            clientSocket.Send(encode.GetBytes(data));
+            Console.WriteLine("Send：" + data);
+            result = Receive(clientSocket, 5000 * 2); //5*2 seconds timeout.  
+            Console.WriteLine("Receive：" + result);
+            DestroySocket(clientSocket);
+            return result;
+        }
+        /// <summary>  
+        /// 接收数据  
+        /// </summary>  
+        /// <param name="socket"></param>  
+        /// <param name="timeout"></param>  
+        /// <returns></returns>  
+        private static string Receive(Socket socket, int timeout)
+        {
+            string result = string.Empty;
+            socket.ReceiveTimeout = timeout;
+            List<byte> data = new List<byte>();
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            try
+            {
+                while ((length = socket.Receive(buffer)) > 0)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        data.Add(buffer[j]);
+                    }
+                    if (length < buffer.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch { }
+            if (data.Count > 0)
+            {
+                result = encode.GetString(data.ToArray(), 0, data.Count);
+            }
+            MessageBox.Show(result);
+            return result;
+        }
+
+        /// <summary>  
+        /// 销毁Socket对象  
+        /// </summary>  
+        /// <param name="socket"></param>  
+        private static void DestroySocket(Socket socket)
+        {
+            if (socket.Connected)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+            }
+            socket.Close();
         }
     }
 }
--- WiFIMsg/NewWindow.cs
+++ WiFIMsg/NewWindow.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -12,6 +13,9 @@ namespace WiFIMsg
 {
     public partial class NewWindow : Form
     {
+
+        private static Encoding encode = Encoding.Default;
+
         public NewWindow()
         {
             InitializeComponent();
@@ -19,8 +23,27 @@ namespace WiFIMsg
 
         private void NewWindow_Load(object sender, EventArgs e)
         {
-
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect("127.0.0.1", 8888);
+            Send("127.0.0.1", 8888, "test");
         }
+        /// <summary>  
+        /// 发送数据  
+        /// </summary>  
+        /// <param name="host"></param>  
+        /// <param name="port"></param>  
+        /// <param name="data"></param>  
+        /// <returns></returns>  
+        public string Send(string host, int port, string data)
+        {
+            string result = string.Empty;
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket.Connect(host, port);
+            clientSocket.Send(encode.GetBytes(data));
+            richTextBox1.AppendText("send: "+data+"\r\n");
+            return result;
+        }
+
 
         protected override void WndProc(ref Message m)
         {
